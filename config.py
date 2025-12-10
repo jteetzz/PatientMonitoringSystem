88da8caa6d9590348013a158a818dfa91cff2a52
amp77777 <amp7777@psu.edu>
2025-12-10 17:22:24 -0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    SECRET_KEY = "change-me"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///healthcare_monitoring.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Alert thresholds (toy configuration)
-    MAX_HEART_RATE = 120
-    MIN_HEART_RATE = 50
-    MIN_SPO2 = 92
