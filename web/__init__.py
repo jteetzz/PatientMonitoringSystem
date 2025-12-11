cb2313a6ee58bd7600f2094926da062b97c77afa
John Teetz <johnttz215@gmail.com>
2025-12-10 22:34:43 -0500

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,3 +1,11 @@
 from flask import Blueprint
 
-web_bp = Blueprint("web", __name__, template_folder="templates", static_folder="static")
+web_bp = Blueprint(
+    "web",
+    __name__,
+    template_folder="templates",  # web/templates
+    static_folder="static",       # web/static
+)
+
+from . import routes  # make sure routes register on web_bp
+
